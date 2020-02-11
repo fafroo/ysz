bd00a4e5b8c6c13273baab10d18694cd3ba91f32
milos <vojta.milos@gmail.com>
2020-02-11 12:47:34 +0100

    meta_run completed; saving files; 1 intermediate sneh-file

--- snehurka/run_ysz_fitting_par_study-prms-.jl
+++ snehurka/run_ysz_fitting_par_study-prms-.jl
@@ -1,10 +1,13 @@
+#!/home/masicko/Downloads/julia-1.3.1/bin/julia
+#
+#SBATCH --job-name=NYQ
+#SBATCH -p express
+#SBATCH --time=0-6
+
+
+#module add julia
+#module --ignore-cache load "julia"
+
 include("../examples/ysz_fitting.jl")
 
-ysz_fitting.par_study(ARGS[1], ARGS[2], ARGS[3], ARGS[4], ARGS[5], ARGS[6], ARGS[7], ARGS[8], ARGS[9])
-
-    # A0_in \in [-6, 6]
-    # R0_in \in [-6, 6]
-    # DGA_in \in [-1, 0]
-    # DGR_in \in [-2, 2]
-    # beta_in \in [0,1]
-    # A_in \in [-1, 1]
+ysz_fitting.par_study_script_wrap(ARGS[1], ARGS[2], ARGS[3], ARGS[4], ARGS[5], ARGS[6], ARGS[7])
