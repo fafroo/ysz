52be7026c45be9fdc3ce7ec7a153786880e5c2a2
milos <vojta.milos@gmail.com>
2020-02-26 13:18:36 +0100

    meta_run_par_study {direct, scripted way}, loading of par_studyes

--- src/EIS_simulation.jl
+++ src/EIS_simulation.jl
@@ -1,5 +1,7 @@
+using CSV
 using DataFrames
 using PyPlot
+
 
 
 
@@ -16,6 +18,8 @@ mutable struct EIS_simulation <: abstract_simulation
   dx_exp::Float64
   omega_range::Tuple
   fig_size::Tuple 
+  #
+  checknodes::Any
   #
   name::String
   
@@ -41,7 +45,9 @@ function EIS_simulation(TC, pO2, bias=0.0; dx_exp=-9, omega_range=EIS_get_shared
         this.omega_range = omega_range
         this.fig_size = fig_size
         #
-        name = string(this)
+        this.checknodes = get_shared_checknodes(this)
+        #
+        this.name = string(this)
         
         push!(output, this)
       end
@@ -59,7 +65,7 @@ function EIS_get_shared_omega_range()
     return omega_range = (1.1, 51000, 1.2)
 end
   
-function get_shared_checknodes(sim::EIS_simulation)
+function get_shared_checknodes(SIM::EIS_simulation)
     return EIS_get_checknodes_geometrical(EIS_get_shared_omega_range()...)
 end
 
@@ -108,19 +114,19 @@ function typical_plot_exp(SIM::EIS_simulation, EIS_df, additional_string="")
   ax.set_aspect(1.0)  
 end
 
-function fitting_report(SIM::EIS_simulation, plot_prms_string, EIS_exp, EIS_sim)
-  println("EIS_fitting error $(experiment_legend(SIM, latex=false)) $(plot_prms_string)  => ", fitnessFunction(SIM, EIS_exp, EIS_sim))
+function fitness_error_report(SIM::EIS_simulation, plot_prms_string, EIS_exp, EIS_sim)
+  println("EIS fitness error $(experiment_legend(SIM, latex=false)) $(plot_prms_string)  => ", fitnessFunction(SIM, EIS_exp, EIS_sim))
 end
 
 
 #######################################
 # relatively static part ##############
 
-function apply_checknodes(SIM::EIS_simulation, EIS_in,checknodes)
+function apply_checknodes(SIM::EIS_simulation, EIS_in, checknodes)
     DataFrame( f = checknodes, Z = EIS_get_Z_values(EIS_in, checknodes))
 end
 
-function load_file_prms(sim::EIS_simulation;save_dir, prms, prms_names=("A0", "R0", "DGA", "DGR", "betaR", "SR"), scripted_tuple=(0, 0, 0, 0, 0, 0), throw_exception=true, verbose=true)
+function load_file_prms(SIM::EIS_simulation; save_dir, prms, prms_names=("A0", "R0", "DGA", "DGR", "betaR", "SR"), scripted_tuple=(0, 0, 0, 0, 0, 0), throw_exception=true, verbose=true)
 
     (out_path, out_name) = filename_format_prms( save_dir=save_dir, prefix="EIS", prms=prms, prms_names=prms_names, scripted_tuple=scripted_tuple)
     
@@ -191,6 +197,107 @@ function EIS_view_experimental_data(TC_list, pO2_list, bias_list; use_checknodes
     end
 end
 
+function EIS_get_Z_values(Q_raw, checknodes)
+    Zx_values = []
+    #println(Q_raw[!, 1])
+    #println(Q_raw[!, 2])
 
+    start_i = 1
+    for row = 1:size(checknodes,1)
+        Ux = checknodes[row]
+        xk = -1
+        i = start_i
+        while i < length(Q_raw[!, 1])
+            #println(Ux, " // ", i, " // [", Q_raw[!, 1][i], ", ", Q_raw[!, 1][i+1], "]")
+            if is_between(Ux, Q_raw[!, 1][i], Q_raw[!, 1][i+1]) 
+                xk = i
+                start_i = i
+                break
+            end
+            i += 1
+        end
+        
+        if xk == -1
+            println("Error: EIS_get_Z_values: Zx ",Ux," is not in Q_raw[!, 1]: ",Q_raw[!, 1])
+            return Exception()
+        end
+        Ik = Q_raw[!, 2][xk]
+        Il = Q_raw[!, 2][xk+1]
+        Ix = Ik + ((Il - Ik)/(Q_raw[!, 1][xk+1] - Q_raw[!, 1][xk])) * (Ux - Q_raw[!, 1][xk])
+        
+        Zx_values = [Zx_values; Ix]
+    end
+    
+    return Zx_values
+end
+
+function linComb(SIM::EIS_simulation, Q1,Q2,lambda)
+    if Q1[!, 1] == Q2[!, 1]
+        return DataFrame(f = Q1.f, Z = Q1.Z.*(1-lambda) .+ Q2.Z.*(lambda))
+    else
+        println("ERROR: linComb_EIS: shape or value mismatch:\n",Q1.Z,"\n",Q2.Z)
+        return Exception()
+    end
+end
+
+function biliComb(SIM::EIS_simulation, Q11,Q12,Q21,Q22,x,y)
+    if Q11[!, 1] == Q12[!, 1] && Q11[!, 1] == Q21[!, 1] && Q11[!, 1] ==Q22[!, 1]
+        return DataFrame(f = Q11[!, 1], 
+             Z = Q11[!, 2].*(1-x).*(1-y) .+ Q21[!, 2].*x.*(1-y) .+ Q12[!, 2].*(1-x).*y .+ Q22[!, 2].*x.*y
+            )
+    else
+        println("ERROR: biliComb_EIS: shape mismatch or different *.f values")
+        return Exception()
+    end
+end
+
+
+function fitnessFunction(SIM::EIS_simulation, exp_EIS::DataFrame, sim_EIS::DataFrame)
+        err = 0.0
+        if  exp_EIS.f == sim_EIS.f
+                
+                for row = 1:size(exp_EIS,1)
+                        err +=( (real(exp_EIS.Z[row]) - real(sim_EIS.Z[row]))^2 
+                                 + (imag(exp_EIS.Z[row]) - imag(sim_EIS.Z[row]))^2)
+                end
+        else
+                println("ERROR: EIS_fitnesFunction: shape mismatch or different *.f values")
+                return Exception()
+        end
+        # returns average error per checknode
+        return sqrt(err)/Float32(size(exp_EIS,1))
+end
+
+
+function EIS_get_checknodes_short()
+    # frequency nodes to compare
+    # must be sorted upwards
+    checknodes = (
+        1.0e+0,
+        1.0e+1,
+        1.0e+2,
+        1.0e+3
+    )
+end
+
+function EIS_get_checknodes_geometrical(start_n, end_n, n_fac)
+    # frequency nodes to compare
+    # must be sorted upwards
+    w_list = zeros(0)
+    w = start_n
+    if start_n == end_n
+            append!(w_list, start_n)
+            return w_list
+    end
+    if n_fac <= 1 || start_n > end_n
+        println("ERROR: get_checknodes: n_fac <= 1 || start_n > end_n")
+        return throw(Exception)
+    end
+    while w < end_n
+        append!(w_list, w)
+        w *= n_fac
+    end    
+    return w_list
+end
 
 
